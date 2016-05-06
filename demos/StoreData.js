bf9e81fd351c130177e088de65b386ac4068a0ef
Sebastian Frey <sebastian.frey@outlook.com>
2016-05-06 15:06:39 +0200

    redefine store data

--- demos/StoreData.js
+++ demos/StoreData.js
@@ -1,31 +1,68 @@
 define({
-  "data": [
+  categories: [
     {
-      "type": "textbox",
-      "label": "Textbox 1",
-      "args": {
-        "value": "wert1"
-      }
+      label: "Custom",
+      id: 1,
+      elements: [
+        {
+          type: "textbox",
+          label: "Textbox 1",
+          args: {
+            name: 'firstvalue',
+            value: "value 1"
+          }
+        }, {
+          type: "textbox",
+          label: "Textbox 2",
+          args: {
+            name: 'secondvalue',
+            value: "value 2"
+          }
+        }, {
+          type: "numberspinner",
+          label: "Numberspinner",
+          args: {
+            name: 'thirdvalue',
+            value: 1000,
+            smallDelta: 10,
+            constraints: { "min":9, "max":1550, "places":0 }
+          }
+        }, {
+          type: "checkbox",
+          label: "Numberspinner",
+          args: {
+            name: 'lastvalue',
+            checked: true
+          }
+        }
+      ], 
     }, {
-      "type": "textbox",
-      "label": "Textbox 2",
-      "args": {
-        "value": "wert2"
-      }
-    }, {
-      "type": "numberspinner",
-      "label": "Numberspinner",
-      "args": {
-        "value": 1000,
-        "smallDelta": 10,
-        "constraints": { "min":9, "max":1550, "places":0 }
-      }
-    }, {
-      "type": "checkbox",
-      "label": "Numberspinner",
-      "args": {
-        "checked": true
-      }
+      label: "Contact",
+      id: 2,
+      elements: [
+        {
+          type: "textbox",
+          label: "Last name",
+          args: {
+            name: 'first',
+            placeholder: "Your last name"
+          }
+        }, {
+          type: "textbox",
+          label: "First name",
+          args: {
+            name: 'last',
+            placeholder: "Your first name"
+          }
+        }, {
+          type: "datetextbox",
+          label: "Birth date",
+          args: {
+            name: 'birth',
+            value: ""
+          }
+        }
+      ]
     }
   ]
 });

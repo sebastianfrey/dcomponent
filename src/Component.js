c81f9fe8d3aa4ac2ce0f25ac5baea8e51cf56093
Sebastian Frey <sebastian.frey@outlook.com>
2016-05-06 15:10:05 +0200

    set base class in Component

--- src/Component.js
+++ src/Component.js
@@ -7,5 +7,7 @@ define([
   './dijit/_StoreMixin',
   './dijit/_FactoryMixin'
 ], function(declare, domConstruct, _WidgetBase, _StoreMixin, _FactoryMixin) {
-  return declare('dcomponent.Component', [ _WidgetBase, _StoreMixin, _FactoryMixin]);
+  return declare('dcomponent.Component', [ _WidgetBase, _StoreMixin, _FactoryMixin], {
+    baseClass: 'dcomponent'
+  });
 });
